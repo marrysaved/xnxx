3bc14c18f38c0112b623e6022fbe6ef4f922b003
marrysaved <55679494+marrysaved@users.noreply.github.com>
2020-04-23 17:17:34 +0800

    Create xnxx1.js

---
+++ xnxx1.js
@@ -0,0 +1,50 @@
+
+document.writeln( '<li><a href="http://ad1.xxbvideo.com">含羞草高清视频</a></li>');
+document.writeln( '<li><a href="https://700wav.best">700万AV</a></li>');
+document.writeln( '<li><a href="https://800w.bestxfilm.xyz">800万AV</a></li>');
+document.writeln( '<li><a href="https://pornhubfreeporn.xyz">Pornhub</a></li>');
+document.writeln( '<li><a href="https://freehdxvideosporn.xyz">Xvideos</a></li>');
+document.writeln( '<li><a href="https://xnxxfreeporn.xyz">Xnxx</a></li>');
+document.writeln( '<li><a href="https://youjizzfreeporn.xyz">Youjizz</a></li>');
+document.writeln( '<li><a href="https://xxbvideo.com">复仇者淫盟</a></li>');
+document.writeln( '<li><a href="https://2048.www1024com.xyz">2048核基地</a></li>');
+document.writeln( '<li><a href="https://www1024com.xyz">1024核工厂</a></li>');
+ 
+var tags=new Array("性感","3P","约炮","口交","情趣","丝袜","巨乳","后入","激情","基本式","学生","夫妻","少妇","熟女","女上位","酒店","KTV","群P","双飞","自慰","好身材","办公室","乳交","浴室","舔逼","约炮舔逼","制服","丰满","姐妹","激情0","按摩","健身房","清纯","道具","车震","打飞机","出租屋","学生自慰","可爱","护士","直播","极品美女","肛交","69","性感少妇","sm","动画","卫生间","巨屌","户外","模特","dm","白领","基口交","仓库","空姐","温泉","乱伦","偷拍","厨房","片段","媳妇","超级","多种","推车式","色狼","诱惑","自己的","港台三级","性感少妇","抱式","偷拍","白嫩","无套","大家","一流","国产自拍","喝醉","高清","健身房","陌生人","难耐","新片","男神","出租房","豪宅","鸡鸡","担肩式","老婆","叫声","炮友","电影","眼镜妹","汽车","母狗","火爆","紧身衣","足交","福利","超浪","女学生","小姨子","阳具","外传","华裔","视频","嫖妓","主动","搭讪","洗澡","短裙","特写","秘密","艺校","大哥","摩擦","同事","规则","宅男");
+
+
+domain = new Array();
+
+domain[0] = "https://700wav.best/search/";
+domain[1] = "https://800w.bestxfilm.xyz/?k=";
+domain[2] = "https://pornhubfreeporn.xyz/video/search?search=";
+domain[3] = "https://pornhubapp.xyz/video/search?search=";
+domain[4] = "https://freehdxvideosporn.xyz/?k=";
+domain[5] = "https://xnxxfreeporn.xyz/search/";
+domain[6] = "https://youjizzfreeporn.xyz/video/search?search=";
+domain[7] = "https://qqavjzy.xxbvideo.com/?k=";
+domain[8] = "https://best-xvideos-freeporn.com/?k=";
+domain[9] = "https://xvideoshub.best-xvideos-freeporn.com/video/search?search=";
+domain[10] = "https://cut1.bestxfilm.xyz/search/";
+domain[11] = "https://voyeur.chinaporn.xyz/search/";
+domain[12] = "https://jmmdh.xyz/?k=";
+domain[13] = "https://fcw.jmmdh.xyz/?k=";
+domain[14] = "https://porn.jmmdh.xyz/?k=";
+domain[15] = "https://video.jmmdh.xyz/?k=";
+domain[16] = "https://xp1024fuli.com/search/";
+domain[17] = "https://avhub.jmmdh.xyz/search/";
+domain[18] = "https://18teenpornsex.xyz/video/search?search=";
+  
+for(var i=0;i< domain.length*3;i++){
+
+l = Math.random() * domain.length;
+l= Math.floor(l);
+ 
+t= Math.random() * tags.length;
+t= Math.floor(t);
+ 
+document.writeln( '<li><a href='+domain[l]+tags[t]+'>'+tags[t]+'</a></li>');
+		 
+		 
+}
+
