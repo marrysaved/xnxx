7b75280f9a5923411e93c75f852e17654f722da8
marrysaved <55679494+marrysaved@users.noreply.github.com>
2020-07-29 08:03:53 +0800

    Update xnxx1.js

--- xnxx1.js
+++ xnxx1.js
@@ -4,11 +4,11 @@ document.writeln( '<li><a href="https://700wav.best">700万AV</a></li>');
 document.writeln( '<li><a href="https://800w.bestxfilm.xyz">800万AV</a></li>');
 document.writeln( '<li><a href="https://pornhdxvideos.xyz">Pornhub</a></li>');
 document.writeln( '<li><a href="https://freehdxvideosporn.xyz">Xvideos</a></li>');
-document.writeln( '<li><a href="https://xnxxfreeporn.xyz">Xnxx</a></li>');
+document.writeln( '<li><a href="https://xnxx2.gq">Xnxx</a></li>');
 document.writeln( '<li><a href="https://bestfreeporntube.xyz">Youjizz</a></li>');
 document.writeln( '<li><a href="https://sexscenes.xyz">复仇者淫盟</a></li>');
-document.writeln( '<li><a href="https://2048.www1024com.xyz">2048核基地</a></li>');
-document.writeln( '<li><a href="https://www1024com.xyz">1024核工厂</a></li>');
+document.writeln( '<li><a href="https://3some.ml" target="_blank">三人行</a></li>');
+document.writeln( '<li><a href="https://ph2020.xyz" target="_blank">洋马视频</a></li>');
  
 var tags=new Array("性感","3P","约炮","口交","情趣","丝袜","巨乳","后入","激情","基本式","学生","夫妻","少妇","熟女","女上位","酒店","KTV","群P","双飞","自慰","好身材","办公室","乳交","浴室","舔逼","约炮舔逼","制服","丰满","姐妹","激情0","按摩","健身房","清纯","道具","车震","打飞机","出租屋","学生自慰","可爱","护士","直播","极品美女","肛交","69","性感少妇","sm","动画","卫生间","巨屌","户外","模特","dm","白领","基口交","仓库","空姐","温泉","乱伦","偷拍","厨房","片段","媳妇","超级","多种","推车式","色狼","诱惑","自己的","港台三级","性感少妇","抱式","偷拍","白嫩","无套","大家","一流","国产自拍","喝醉","高清","健身房","陌生人","难耐","新片","男神","出租房","豪宅","鸡鸡","担肩式","老婆","叫声","炮友","电影","眼镜妹","汽车","母狗","火爆","紧身衣","足交","福利","超浪","女学生","小姨子","阳具","外传","华裔","视频","嫖妓","主动","搭讪","洗澡","短裙","特写","秘密","艺校","大哥","摩擦","同事","规则","宅男");
 
@@ -20,7 +20,7 @@ domain[1] = "https://800w.bestxfilm.xyz/?k=";
 domain[2] = "https://pornhdxvideos.xyz/video/search?search=";
 domain[3] = "https://pornhubapp.xyz/video/search?search=";
 domain[4] = "https://freehdxvideosporn.xyz/?k=";
-domain[5] = "https://xnxxfreeporn.xyz/search/";
+domain[5] = "https://xnxx2.gq/search/";
 domain[6] = "https://bestfreeporntube.xyz/video/search?search=";
 domain[7] = "https://qqavjzy.sexscenes.xyz/?k=";
 domain[8] = "https://best-xvideos-freeporn.com/?k=";
